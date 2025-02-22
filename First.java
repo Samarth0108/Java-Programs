abc02702fdb5f216e0648ba4f7357bb7337f9e53
Samarth Agrawal <138054858+Samarth0108@users.noreply.github.com>
2025-02-22 11:05:27 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,9 @@
+class First{
+              //default access modifier
+              //public in same package/ private in diff. package
+              // out is in built obj declared in sys class
+              // println is a printing stat
+    public static void main(String args[]){
+        System.out.println("Hello ! World.");
+    }
+}
