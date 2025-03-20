aa129f85c47e03db0878eefd0280c0e9fa34dd99
Samarth Agrawal <138054858+Samarth0108@users.noreply.github.com>
2025-03-20 09:02:00 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,11 @@
+class Solution{
+    public static void main(int n){
+        int i,j;
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
